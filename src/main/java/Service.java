2f89d5d653c4468be70863da4f26e47097856499
Mykola Lehkyi <lintey8@gmail.com>
2020-02-20 23:04:33 +0200

    profit apdated

--- src/main/java/Service.java
+++ src/main/java/Service.java
@@ -1,14 +1,25 @@
+import java.util.List;
+
 public class Service {
     private Database database;
-    static private Integer daysToDeadline;
+    static private Integer daysPast=0;
 
 
     public void result(){
-        daysToDeadline=database.getDaysForScanning();
-    for ()
+        List<Library> libraries = database.getLibraries();
+        for ( int i =0; i<libraries.size();i++){
+            calculateProfit(libraries.get(i));
+        }
+
     }
 
     public void calculateProfit(Library library){
+        Integer sendDays=database.getDaysForScanning()-library.getSignupDays()-daysPast;
+        List<Book> books = library.getBooks();
+        library.setProfit(0);
+        for (int i=0;i<sendDays*library.getNumsOfBooksShippedPerDay() && i<books.size();i++){
+            library.setProfit(library.getProfit()+books.get(i).getScore());
+        }
 
     }
 
