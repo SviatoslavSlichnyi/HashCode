8822c6ff796b32030b73db2a029ef9c61cd15b81
Sviatoslav Slichnyi <sslichnyi@gmail.com>
2020-02-20 22:06:40 +0200

    Book and Library were changed.

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,5 +1,17 @@
+import java.util.Objects;
+
 public class Book {
+
     private Integer id;
+    private Integer score;
+
+    public Book() {
+    }
+
+    public Book(Integer id, Integer score) {
+        this.id = id;
+        this.score = score;
+    }
 
     public Integer getId() {
         return id;
@@ -17,5 +29,25 @@ public class Book {
         this.score = score;
     }
 
-    private Integer score;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id) &&
+                Objects.equals(score, book.score);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, score);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", score=" + score +
+                '}';
+    }
 }
--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,12 +1,20 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Library {
     private Integer id;
-    private List<Book> books = new ArrayList<>(100000);
     private Integer numsOfBook;
-    private Integer timeToSignup;
+    private Integer signupDays;
     private Integer numsOfBooksShippedPerDay;
+    private List<Book> books = new ArrayList<>(100000);
+
+    public Library() {
+    }
+
+    public Library(Integer id) {
+        this.id = id;
+    }
 
     public Integer getId() {
         return id;
@@ -32,12 +40,12 @@ public class Library {
         this.numsOfBook = numsOfBook;
     }
 
-    public Integer getTimeToSignup() {
-        return timeToSignup;
+    public Integer getSignupDays() {
+        return signupDays;
     }
 
-    public void setTimeToSignup(Integer timeToSignup) {
-        this.timeToSignup = timeToSignup;
+    public void setSignupDays(Integer signupDays) {
+        this.signupDays = signupDays;
     }
 
     public Integer getNumsOfBooksShippedPerDay() {
@@ -46,5 +54,33 @@ public class Library {
 
     public void setNumsOfBooksShippedPerDay(Integer numsOfBooksShippedPerDay) {
         this.numsOfBooksShippedPerDay = numsOfBooksShippedPerDay;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Library library = (Library) o;
+        return Objects.equals(id, library.id) &&
+                Objects.equals(numsOfBook, library.numsOfBook) &&
+                Objects.equals(signupDays, library.signupDays) &&
+                Objects.equals(numsOfBooksShippedPerDay, library.numsOfBooksShippedPerDay) &&
+                Objects.equals(books, library.books);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, numsOfBook, signupDays, numsOfBooksShippedPerDay, books);
+    }
+
+    @Override
+    public String toString() {
+        return "Library{" +
+                "id=" + id +
+                ", numsOfBook=" + numsOfBook +
+                ", signupDays=" + signupDays +
+                ", numsOfBooksShippedPerDay=" + numsOfBooksShippedPerDay +
+                ", books=" + books +
+                '}';
     }
 }
